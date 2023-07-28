d295e45fa75de5b68e0cda2b5bbf47eaece31f2a
funayamateppei <t.funayama0217@gmail.com>
2023-07-28 14:10:03 +0900

    1週間の天気情報を表示

---
+++ src/components/WeekWeatherBox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "../styles/components/WeekWeatherBox.module.css";
+
+const WeekWeatherBox = ({ day, pop, weather, children }) => {
+  return (
+    <div className={styles.weatherContainer}>
+      <div className={styles.dateBox}>{day}</div>
+      <div className={styles.weatherBox}>
+        {weather}
+        {children}
+      </div>
+      <div className={styles.popBox}>{pop == " %" ? "- %" : pop}</div>
+    </div>
+  );
+};
+
+export default WeekWeatherBox;
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import styles from "../styles/detail.module.css";
 import { useSearchParams } from "react-router-dom";
 import defaultAxios from "../lib/axios";
 import useSWR from "swr";
 import LocationBackButton from "../components/LocationBackButton";
+import WeekWeatherBox from "../components/WeekWeatherBox";
 
 const Detail = () => {
   const [searchParams] = useSearchParams();
@@ -19,11 +21,64 @@ const Detail = () => {
   }, []);
   console.log(data);
 
+  const currentDate = new Date();
+  const month = currentDate.getMonth() + 1;
+  const date = currentDate.getDate();
+  const day = currentDate.getDay();
+  const weekday = ["日", "月", "火", "水", "木", "金", "土"];
+
   return (
     <div>
       <LocationBackButton
         route={`/result?prefecture_code=${prefecture_code}`}
       />
+
+      <div className={styles.container}>
+        <div className={styles.weatherDetailContainer}>
+          {data ? (
+            <>
+              <h3>{data.Detail.area.name}</h3>
+            </>
+          ) : (
+            <div className={styles.loadingText}>
+              <h3>Loading...</h3>
+            </div>
+          )}
+        </div>
+
+        <div className={styles.weekWeather}>
+          {data ? (
+            <>
+              <h3>{`${data.Week[0].area.name}の天気予報`}</h3>
+              <p>{`本日${month}月${date}日(${weekday[day]})`}</p>
+              <div className={styles.weekWeatherContainer}>
+                <WeekWeatherBox
+                  day={"日付"}
+                  weather={"天気"}
+                  pop={"降水確率(%)"}
+                />
+                {data.Week[0].weatherCodes.map((detail, index) => (
+                  <WeekWeatherBox
+                    key={index}
+                    day={detail.date}
+                    weather={detail.weather}
+                    pop={`${detail.pop} %`}
+                  >
+                    <img
+                      src={`https://www.jma.go.jp/bosai/forecast/img/${detail.image_code}`}
+                      alt="icon"
+                    />
+                  </WeekWeatherBox>
+                ))}
+              </div>
+            </>
+          ) : (
+            <div className={styles.loadingText}>
+              <h3>Loading...</h3>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
