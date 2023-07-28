8ed0e04b5fb1f8485e9faa6809905771626b8e7c
funayamateppei <t.funayama0217@gmail.com>
2023-07-28 17:55:25 +0900

    詳細画面実装

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -27,6 +27,17 @@ const Detail = () => {
   const day = currentDate.getDay();
   const weekday = ["日", "月", "火", "水", "木", "金", "土"];
 
+  const popsTime = [
+    "00~06",
+    "06~12",
+    "12~18",
+    "18~24",
+    "00~06",
+    "06~12",
+    "12~18",
+    "18~24",
+  ];
+
   return (
     <div>
       <LocationBackButton
@@ -38,6 +49,79 @@ const Detail = () => {
           {data ? (
             <>
               <h2>{data.Detail.area.name}</h2>
+
+              <div className={styles.detailContainer}>
+                {/* 日付 */}
+                <div className={styles.dateBox}>
+                  <div className={styles.tableRow}>日付</div>
+                  {data.Detail.weatherCodes
+                    .slice(0, 2)
+                    .map((weather, index) => (
+                      <div key={index} className={styles.date}>
+                        {weather.date}
+                      </div>
+                    ))}
+                </div>
+
+                {/* 天気 */}
+                <div className={styles.weatherContainer}>
+                  <div
+                    className={`${styles.tableRow} ${styles.weatherTableRow}`}
+                  >
+                    天気
+                  </div>
+                  {data.Detail.weatherCodes
+                    .slice(0, 2)
+                    .map((weather, index) => (
+                      <div key={index} className={styles.weatherBox}>
+                        {weather.weather}
+                        <img
+                          src={`https://www.jma.go.jp/bosai/forecast/img/${weather.image_code}`}
+                          alt="icon"
+                        />
+                      </div>
+                    ))}
+                </div>
+
+                {/* 風 */}
+                <div className={styles.windBox}>
+                  <div className={styles.tableRow}>風</div>
+                  {data.Detail.waves.slice(0, 2).map((wave, index) => (
+                    <div className={styles.wind} key={index}>
+                      {wave}
+                    </div>
+                  ))}
+                </div>
+
+                {/* 波 */}
+                <div className={styles.waveBox}>
+                  <div className={styles.tableRow}>波</div>
+                  {data.Detail.winds.slice(0, 2).map((wind, index) => (
+                    <div className={styles.wave} key={index}>
+                      {wind}
+                    </div>
+                  ))}
+                </div>
+
+                {/* 降水確率 */}
+                <div className={styles.popsContainer}>
+                  <div className={`${styles.tableRow} ${styles.popsTableRow}`}>
+                    降水確率(%)
+                  </div>
+                  <div className={styles.popsBox}>
+                    <div className={styles.pops}>
+                      {popsTime.map((time, index) => (
+                        <div key={index}>{time}</div>
+                      ))}
+                    </div>
+                    <div className={styles.pops}>
+                      {data.Detail.pops.map((pop, index) => (
+                        <div key={index}>{pop}%</div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </>
           ) : (
             <div className={styles.loadingText}>
